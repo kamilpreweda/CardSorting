1c0c0b729f7aa61fa3321fe0c90707b235ed2e44
Kamil Preweda <preweda.kamil@o2.pl>
2022-06-06 09:53:40 +0200

    Added 'CardComparerByValue' and UI

--- CardSorting/Program.cs
+++ CardSorting/Program.cs
@@ -24,6 +24,17 @@ namespace CardSorting
         static void Main(string[] args)
         {
             List<Card> cards = new List<Card>();
+
+            Console.WriteLine("Podaj liczbę kart: ");
+            if (int.TryParse(Console.ReadLine(), out int numberOfCards));
+                for (int i = 0; i < numberOfCards; i++)
+                    cards.Add(RandomCard());
+            PrintCards(cards);
+
+
+            cards.Sort(new CardComparerByValue());
+            Console.WriteLine("\n...Sortowanie kart...\n");
+            PrintCards(cards);
         }
     }
 }
