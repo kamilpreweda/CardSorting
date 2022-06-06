54648ef9e2b56d59122b1c91885a9e85cce7f81a
Kamil Preweda <preweda.kamil@o2.pl>
2022-06-06 09:36:38 +0200

    Added 'RandomCard' and 'PrintCards' static methods

--- CardSorting/Program.cs
+++ CardSorting/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace CardSorting
+﻿using Cards;
+
+namespace CardSorting
 {
     public class Program
     {
+
+        private static readonly Random random = new Random();
+
+        
+        static Card RandomCard()
+        {
+            return new Card((Values)random.Next(1, 14), (Suits)random.Next(4));
+        }
+
+        static void PrintCards(List<Card> cards)
+        {
+            foreach (Card card in cards)
+            {
+                Console.WriteLine(card.Name);
+            }
+        }
+
         static void Main(string[] args)
         {
-            
+            List<Card> cards = new List<Card>();
         }
     }
 }
